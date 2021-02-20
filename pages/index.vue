93e7ed95836a58cb355a2937991b4dcd0a2be70f
MeatReed <tim.oumpho@gmail.com>
2021-02-20 11:31:18 +0100

    init

---
+++ pages/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col align="center">
+        <v-btn @click="reload">Reload</v-btn>
+      </v-col>
+    </v-row>
+    <v-row v-if="!$fetchState.pending">
+      <v-img :src="image.url" />
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      image: '',
+    }
+  },
+  async fetch() {
+    this.image = await this.$axios.$get(
+      'https://www.nekos.life/api/v2/img//wallpaper'
+    )
+  },
+  methods: {
+    reload() {
+      this.$fetch()
+    },
+  },
+}
+</script>
